549b01cd5c4055e81fab0f9e6fe89001f5f46584
vantamm27 <vantamm27@gmail.com>
2019-05-30 11:37:06 +0700

    Update install_k8s.sh

--- script/install_k8s.sh
+++ script/install_k8s.sh
@@ -10,3 +10,7 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  >> /etc/apt/sources
 sudo apt-get update 
 
 sudo apt-get install -y kubelet=1.11.7-00 kubectl=1.11.7-00 kubeadm=1.11.7-00 kubernetes-cni=0.6.0-00
+
+echo  "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf
+echo  "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.conf
+echo  "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
