f3dd237ffb1bbdd923e801ea762a9c2ff6980393
vantamm27 <vantamm27@gmail.com>
2019-05-30 11:37:38 +0700

    Update install_k8s.sh

--- script/install_k8s.sh
+++ script/install_k8s.sh
@@ -7,9 +7,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"  >> /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update 
-
-sudo apt-get install -y kubelet=1.11.7-00 kubectl=1.11.7-00 kubeadm=1.11.7-00 kubernetes-cni=0.6.0-00
+apt-get update 
+apt-get install -y kubelet=1.11.7-00 kubectl=1.11.7-00 kubeadm=1.11.7-00 kubernetes-cni=0.6.0-00
 
 echo  "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf
 echo  "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.conf
