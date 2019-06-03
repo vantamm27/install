622d821c4c53e87671ddf4c401ab263441b0a7ed
vantamm27 <vantamm27@gmail.com>
2019-06-03 11:35:06 +0700

    Delete install_zabbit.sh

--- script/install_zabbit.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/vantamm27/install/raw/master/script/zabbix30-agent-deb.tar.gz
-
-tar -xvf zabbix30-agent-deb.tar.gz
-
-cd  zabbix30-agent-deb
-
-chmod +x install-deb.sh
-
-./install-deb.sh  118.102.5.141
