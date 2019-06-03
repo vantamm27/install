4dcd676646197b1cf32e5950a533c9fb2b256349
vantamm27 <vantamm27@gmail.com>
2019-06-03 11:32:26 +0700

    Update install_zabbit.sh

--- script/install_zabbit.sh
+++ script/install_zabbit.sh
@@ -2,7 +2,7 @@ wget https://github.com/vantamm27/install/raw/master/script/zabbix30-agent-deb.t
 
 tar -xvf zabbix30-agent-deb.tar.gz
 
-cd  tar -xvf zabbix30-agent-deb.tar.gz
+cd  zabbix30-agent-deb
 
 chmod +x install-deb.sh
 
